d3ea8e768efc402c5fe5763d59a8bbbfad4b9c36
lutianyu.990919 <lutianyu.990919@bytedance.com>
2023-01-31 11:29:26 +0800

    feat(project setting): 新增配置

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
-}
+  extends: ['@commitlint/config-conventional'],
+  helpUrl: 'https://www.baidu.com',
+  rules: {
+    // "type-enum": [2, 'always', ['傻逼', '一贤']]
+    // "scope-enum": [2, 'always', ['body']]
+    'scope-empty': [2, 'never'], // * 不能为空
+    'subject-empty': [2, 'never'] // * 不能为空
+  }
+};
