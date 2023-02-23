bdf76b3051f9545f42cd5e3d84396fa5d510904e
lutianyu.990919 <1141049271@qq.com>
2023-02-23 19:33:25 +0800

    feat(layer): 新增layer插件

---
+++ src/utils/layer/index.ts
@@ -0,0 +1,128 @@
+/*
+ * @Author: tianyu
+ * @Date: 2023-02-23 16:39:51
+ * @Description:
+ */
+
+import './index.less';
+class OpenImgMask {
+  type: 'Horizontal' | 'Vertical';
+  multiple: number; // * 放大倍数
+  constructor() {
+    this.type = 'Horizontal';
+    this.multiple = 0;
+  }
+
+  render(selector: string): void {
+    const ImgItems = document.querySelectorAll(selector);
+    ImgItems.forEach((item) => {
+      item.addEventListener('click', (ev: Event) => {
+        const w = window.innerWidth;
+        const h = window.innerHeight;
+        let type = 'Horizontal';
+
+        const target = ev.target as HTMLElement;
+        if (target) {
+          const { offsetWidth, offsetHeight, offsetLeft, offsetTop, tagName } = target;
+          console.dir(target);
+          console.log(target.offsetLeft);
+          // * 如果宽度小于高度，那么以高度为基准渲染垂直。
+          if (offsetWidth < offsetHeight) {
+            type = 'Vertical';
+          }
+        }
+      });
+    });
+  }
+}
+
+const layer = new OpenImgMask();
+export { layer };
+
+let mask: HTMLDivElement | null = null;
+let contentElement: HTMLElement | null = null;
+
+// * 删除
+function removeLayer(): void {
+  document.body.classList.remove('layer-body');
+  if (contentElement) {
+    contentElement.style.transform = '';
+  }
+  setTimeout(() => {
+    if (mask) {
+      document.body.removeChild(mask);
+      mask = null;
+    }
+    if (contentElement) {
+      document.body.removeChild(contentElement);
+      contentElement = null;
+    }
+  }, 300);
+}
+
+function createMask(): void {
+  const dom = document.createElement('div');
+  mask = dom;
+  dom.classList.add('layer-mask');
+  dom.addEventListener('click', () => {
+    removeLayer();
+  });
+  // * 添加
+  setTimeout(() => {
+    document.body.classList.add('layer-body');
+  });
+  document.body.appendChild(dom);
+}
+
+function open(multiple: number, target: HTMLElement, type: 'Horizontal' | 'Vertical' = 'Horizontal'): void {
+  const { offsetWidth, offsetHeight, offsetLeft, offsetTop, tagName } = target;
+  console.dir(target);
+  const top = offsetTop - document.documentElement.scrollTop;
+  const left = offsetLeft - document.documentElement.scrollLeft;
+  const halfWidth = (multiple * offsetWidth - offsetWidth) / 2;
+  const translateX = (offsetWidth - halfWidth) / multiple;
+  console.log(multiple, halfWidth, translateX);
+  console.log(halfWidth / multiple);
+
+  createMask();
+  const content = document.createElement(tagName);
+  document.body.appendChild(content);
+  setTimeout(() => {
+    content.style.cssText = `
+      transform: scale(${multiple}) translate3d(-${translateX}px, ${0}px, 0px); 
+      position: fixed; 
+      left: ${left}px; top: ${top}px;
+      width: ${offsetWidth}px; 
+      height: ${offsetHeight}px; z-index: 100;
+    `;
+  });
+  content.classList.add('layer-content');
+  contentElement = content;
+  contentElement.addEventListener('click', () => {
+    removeLayer();
+  });
+}
+
+export function openImgMask(selector: string): void {
+  const ImgItems = document.querySelectorAll(selector);
+  let multiple = 0; // * 放大倍数
+  ImgItems.forEach((item) => {
+    item.addEventListener('click', (ev: Event) => {
+      const docWidth = window.innerWidth;
+      const docHeight = window.innerHeight;
+
+      const target = ev.target as HTMLElement;
+      if (target) {
+        const { offsetWidth, offsetHeight } = target;
+        console.log(target.offsetLeft);
+        if (offsetWidth > offsetHeight) {
+          multiple = docWidth / offsetWidth;
+          open(multiple, target);
+          return;
+        }
+        multiple = docHeight / offsetHeight;
+        open(multiple, target, 'Vertical');
+      }
+    });
+  });
+}
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button } from 'antd';
 import { useNavigate, Outlet } from 'react-router-dom';
+import './index.less';
+import { layer, openImgMask } from 'src/utils/layer';
 
 export default function Home() {
   const navigate = useNavigate();
+  useEffect(() => {
+    openImgMask('.imgMask');
+  }, []);
   return (
     <>
       <div>Home</div>
@@ -14,6 +19,33 @@ export default function Home() {
       >
         To Me
       </Button>
+
+      <div className="imgMaskContainer">
+        <div className="item">
+          <div className="left"></div>
+          <div className="imgMask">1</div>
+        </div>
+        <div className="item">
+          <div className="left"></div>
+          <div className="imgMask">2</div>
+        </div>
+        <div className="item">
+          <div className="left"></div>
+          <div className="imgMask">3</div>
+        </div>
+        <div className="item">
+          <div className="left"></div>
+          <div className="imgMask">4</div>
+        </div>
+        <div className="item">
+          <div className="left"></div>
+          <div className="imgMask">5</div>
+        </div>
+        <div className="item">
+          <div className="left"></div>
+          <div className="imgMask">6</div>
+        </div>
+      </div>
       <Outlet />
     </>
   );
